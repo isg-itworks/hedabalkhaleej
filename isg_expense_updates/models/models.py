eaa9ef859fa1521fd6812a738e9f52abe726b6f2
alzahraagamal1 <alzahraagamal1@gmail.com>
2021-05-02 18:00:25 +0200

    expense updates of 1/5

--- isg_expense_updates/models/models.py
+++ isg_expense_updates/models/models.py
@@ -6,8 +6,16 @@ from odoo.tools import float_is_zero
 
 class IsgExpenseUpdates(models.Model):
     _inherit = 'hr.expense'
-    analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic Account', states={'post': [('readonly', True)], 'done': [('readonly', True)]}, oldname='analytic_account')
-
+    analytic_account_id = fields.Many2one('account.analytic.account', string='Analytic Account', states={'post': [('readonly', True)],'approved': [('required', True)], 'done': [('readonly', True)]}, oldname='analytic_account')
+    state = fields.Selection([
+        ('draft', 'To Submit'),
+        ('reported', 'Submitted'),
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('done', 'Paid'),
+        ('refused', 'Refused')
+    ], compute='_compute_state', string='Status', copy=False, index=True, readonly=True, store=True,
+        help="Status of the expense.")
     @api.model
     def _get_employee_id_domain(self):
         res = [('id', '=', 0)]  # Nothing accepted by domain, by default
@@ -29,6 +37,22 @@ class IsgExpenseUpdates(models.Model):
             res = [('id', '=', employee.id)]
         return []
 
+    @api.depends('sheet_id', 'sheet_id.account_move_id', 'sheet_id.state')
+    def _compute_state(self):
+        for expense in self:
+            if not expense.sheet_id or expense.sheet_id.state == 'draft':
+                expense.state = "draft"
+            elif expense.sheet_id.state == "cancel":
+                expense.state = "refused"
+            elif expense.sheet_id.state == "pending":
+                expense.state = "pending"
+            elif expense.sheet_id.state == "approve" or expense.sheet_id.state == "post":
+                expense.state = "approved"
+            elif not expense.sheet_id.account_move_id:
+                expense.state = "reported"
+            else:
+                expense.state = "done"
+
 
 class IsgExpenseUpdatesSheet(models.Model):
     _inherit = 'hr.expense.sheet'
@@ -36,18 +60,28 @@ class IsgExpenseUpdatesSheet(models.Model):
     user_id = fields.Many2one('res.users', 'Manager',store=True,  readonly=True,compute='_onchange_employee_id', copy=False, states={'draft': [('readonly', False)]}, track_visibility='onchange', oldname='responsible_id')
     expense_line_ids = fields.One2many('hr.expense', 'sheet_id', string='Expense Lines', states={'approve': [('readonly', False)], 'done': [('readonly', True)], 'post': [('readonly', True)]}, copy=False)
     can_approve = fields.Boolean(compute='compute_can_approve')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('submit', 'Submitted'),
+        ('pending', 'Pending'),
+        ('approve', 'Approved'),
+        ('post', 'Posted'),
+        ('done', 'Paid'),
+        ('cancel', 'Refused')
+    ], string='Status', index=True, readonly=True, track_visibility='onchange', copy=False, default='draft',
+        required=True, help='Expense Report State')
 
     def compute_can_approve(self):
         for record in self:
-            if record.state=='submit' and record.env.user == record.user_id:
+            if record.state=='submit' and (record.env.user == record.user_id or self.user_has_groups('hr_expense.group_hr_expense_user')) :
                 record.can_approve = True
             else :
                 record.can_approve= False
 
     @api.multi
-    def approve_expense_sheets(self):
-        self.write({'state': 'approve'})
-        self.activity_update()
+    def confirm_expense_sheets(self):
+        self.write({'state': 'pending'})
+        # self.activity_update()
 
     @api.multi
     def action_sheet_move_create(self):
@@ -56,7 +90,8 @@ class IsgExpenseUpdatesSheet(models.Model):
 
         if any(not sheet.journal_id for sheet in self):
             raise UserError(_("Expenses must have an expense journal specified to generate accounting entries."))
-
+        if any(not sheet.analytic_account_id for sheet in self.expense_line_ids):
+            raise UserError(_("Expenses must have an analytic account to generate accounting entries."))
         expense_line_ids = self.mapped('expense_line_ids')\
             .filtered(lambda r: not float_is_zero(r.total_amount, precision_rounding=(r.currency_id or self.env.user.company_id.currency_id).rounding))
         res = expense_line_ids.sudo().action_move_create()
@@ -70,6 +105,11 @@ class IsgExpenseUpdatesSheet(models.Model):
             self.write({'state': 'done'})
         self.activity_update()
         return res
+
+    @api.multi
+    def approve_expense_sheets(self):
+        self.write({'state': 'approve'})
+        self.activity_update()
 
     @api.onchange('employee_id')
     @api.depends('employee_id')
